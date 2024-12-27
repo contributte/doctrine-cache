cd2d43aa986ad137edcc86ae7bd1d81f0e80d84c
Jan Trejbal <jan@trejbal.land>
2024-12-27 20:26:20 +0100

    Create cache adapter service

--- src/DI/CacheExtension.php
+++ src/DI/CacheExtension.php
@@ -13,6 +13,7 @@ use Nette\InvalidStateException;
 use Nette\Schema\Expect;
 use Nette\Schema\Schema;
 use stdClass;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\Cache\Adapter\ApcuAdapter;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
@@ -34,7 +35,7 @@ final class CacheExtension extends CompilerExtension
 	public function getConfigSchema(): Schema
 	{
 		return Expect::structure([
-			'driver' => Expect::anyOf(
+			'adapter' => Expect::anyOf(
 				Expect::string(),
 				Expect::array(),
 				Expect::type(Statement::class)
@@ -47,54 +48,41 @@ final class CacheExtension extends CompilerExtension
 		$builder = $this->getContainerBuilder();
 		$config = $this->config;
 
-		if ($config->driver === null) {
+		if ($config->adapter === null) {
 			// auto choose
-			$driverDefinition = $builder->addDefinition($this->prefix('driver'))
-				->setType(Cache::class);
+			$adapterDefinition = $builder->addDefinition($this->prefix('adapter'))
+				->setType(AdapterInterface::class);
 
 			if ($this->debugMode === true) {
-				/** @phpstan-ignore classConstant.deprecatedClass */
-				if (class_exists(ArrayCache::class)) {
-					/** @phpstan-ignore classConstant.deprecatedClass */
-					$driverDefinition->setFactory(ArrayCache::class);
-				} else {
-					$driverDefinition->setFactory(DoctrineProvider::class . '::wrap', [
-						new Statement(ArrayAdapter::class),
-					]);
-				}
+				$adapterDefinition->setFactory(ArrayAdapter::class);
 			} elseif (function_exists('apcu_exists')) {
-				/** @phpstan-ignore classConstant.deprecatedClass */
-				if (class_exists(ApcuCache::class)) {
-					/** @phpstan-ignore classConstant.deprecatedClass */
-					$driverDefinition->setFactory(ApcuCache::class);
-				} else {
-					$driverDefinition->setFactory(DoctrineProvider::class . '::wrap', [
-						new Statement(ApcuAdapter::class),
-					]);
-				}
+				$adapterDefinition->setFactory(ApcuAdapter::class);
 			} elseif (isset($builder->parameters['tempDir'])) {
-				/** @phpstan-ignore classConstant.deprecatedClass */
-				if (class_exists(PhpFileCache::class)) {
-					/** @phpstan-ignore classConstant.deprecatedClass */
-					$driverDefinition->setFactory(PhpFileCache::class, [
-						$builder->parameters['tempDir'] . '/cache/nettrine.cache',
-					]);
-				} else {
-					$driverDefinition->setFactory(DoctrineProvider::class . '::wrap', [
-						new Statement(FilesystemAdapter::class, [
-							'directory' => $builder->parameters['tempDir'] . '/cache/nettrine.cache',
-						]),
-					]);
-				}
+				$adapterDefinition->setFactory(FilesystemAdapter::class, [
+					'directory' => $builder->parameters['tempDir'] . '/cache/nettrine.cache',
+				]);
 			} else {
 				throw new InvalidStateException(sprintf(
-					'Unable to find an available cache driver, please provide one via \'%s\' configuration.',
-					sprintf('%s > driver', $this->name)
+					'Unable to find an available cache adapter, please provide one via \'%s\' configuration.',
+					sprintf('%s > adapter', $this->name)
 				));
 			}
-		} else {
+
 			$builder->addDefinition($this->prefix('driver'))
-				->setFactory($config->driver);
+				->setType(Cache::class)
+				->setFactory(DoctrineProvider::class . '::wrap', [
+					new Statement($adapterDefinition),
+				]);
+		} else {
+			$adapterDefinition = $builder->addDefinition($this->prefix('adapter'))
+				->setFactory($config->adapter)
+				->setType(AdapterInterface::class);
+
+			$builder->addDefinition($this->prefix('driver'))
+				->setType(Cache::class)
+				->setFactory(DoctrineProvider::class . '::wrap', [
+					new Statement($adapterDefinition),
+				]);
 		}
 	}
 
